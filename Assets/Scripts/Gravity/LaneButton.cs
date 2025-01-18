3069abd8036bef1ac5779207e05d10d3cd37b382
AkuroP <thomas.kang2002@gmail.com>
2025-01-18 15:35:23 -0500

    [UP] Gravity button movement

--- Assets/Scripts/Gravity/LaneButton.cs
+++ Assets/Scripts/Gravity/LaneButton.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class LaneButton : MonoBehaviour
 {
-    [Header("Target Lane & Rotation to have")]
+    [Header("Button Spawn Location")]
     [SerializeField]
-    private CardinalPoint m_cardinalLine;
+    private CardinalPoint m_buttonCardinalLine;
 
+    private CardinalPoint m_targetCardinalPoint;
     [SerializeField]
     private bool m_isLeftLane;
 
@@ -15,12 +16,49 @@ public class LaneButton : MonoBehaviour
     private void Start()
     {
         m_playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        SetButtonCardinalPoint(m_buttonCardinalLine, m_isLeftLane);
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
-        m_playerMovement.GoToLane(m_cardinalLine, m_isLeftLane);
+        m_playerMovement.GoToLane(m_targetCardinalPoint, !m_isLeftLane);
+    }
+
+    //Use after spawning button to know current button location
+    public void SetButtonCardinalPoint(CardinalPoint cardinalPoint, bool isLeft)
+    {
+        m_buttonCardinalLine = cardinalPoint;
+        m_isLeftLane = isLeft;
+        AssignTargetLine();
+    }
+
+    //assign target line depending on button location
+    private void AssignTargetLine()
+    {
+        switch (m_buttonCardinalLine)
+        {
+            case CardinalPoint.NORTH:
+                if (m_isLeftLane) m_targetCardinalPoint = CardinalPoint.EAST;
+                else m_targetCardinalPoint = CardinalPoint.WEST;
+                break;
+
+            case CardinalPoint.SOUTH:
+                if (m_isLeftLane) m_targetCardinalPoint = CardinalPoint.WEST;
+                else m_targetCardinalPoint = CardinalPoint.EAST;
+                break;
+
+            case CardinalPoint.EAST:
+                if (m_isLeftLane) m_targetCardinalPoint = CardinalPoint.SOUTH;
+                else m_targetCardinalPoint = CardinalPoint.NORTH;
+                break;
+
+            case CardinalPoint.WEST:
+                if (m_isLeftLane) m_targetCardinalPoint = CardinalPoint.NORTH;
+                else m_targetCardinalPoint = CardinalPoint.SOUTH;
+                break;
+        }
     }
 
 
